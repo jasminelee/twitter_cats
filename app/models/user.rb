81da63f25faed92141860feda26787f2dd8e92f8
Jasmine <jasminelee@berkeley.edu>
2015-12-04 18:48:30 -0800

    initialize static pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :tweets
+	validates :username, format: {:with => /\A[a-zA-Z0-9]+\z/}, presence: true 
+	validates :email, format: {:with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/}, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205004119) do
+ActiveRecord::Schema.define(version: 20151205012554) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "likes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
